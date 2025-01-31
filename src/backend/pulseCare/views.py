41fe8dab20daf6db10d4f655864fec04b2b94100
Matin_Ghanbari <ghanbarimatin6@gmail.com>
2025-01-31 20:29:10 +0330

    Fix redis port ValueType error

--- src/backend/pulseCare/views.py
+++ src/backend/pulseCare/views.py
@@ -28,8 +28,8 @@ from .models import User, Patient
 # Initialize Redis connection
 redis_client = redis.StrictRedis(
     host=os.getenv('REDIS_HOST'),
-    port=int(os.getenv('REDIS_PORT')),
-    db=int(os.getenv('REDIS_DB'))
+    port=int(str(os.getenv('REDIS_PORT'))),
+    db=int(str(os.getenv('REDIS_DB')))
 )
 
 
